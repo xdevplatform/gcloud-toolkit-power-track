6a715b2cff062a39a735acadbc07d462d8746cfc
prasannacs <prasannacs.dev@gmail.com>
2022-03-15 10:42:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.PORT = 4080;
 
 config.power_track_api  =   {
     'pt_stream_host' : 'gnip-stream.twitter.com',
-    'pt_stream_path' : '<<GET_STREAM_PATH_FROM_GNIP_CONSOLE>>',
+    'pt_stream_path' : '<<GET_STREAM_PATH_FROM_GNIP_CONSOLE>> Example - /stream/powertrack/accounts/{accountName}/publishers/twitter/{stream}.json',
     'gnip_username' : '<<GNIP_USERNAME>>',
     'gnip_password' : '<<GNIP_PASSWORD>>',
     'bearer_token' : 'Bearer <<YOUR_BEARER_TOKEN_HERE>>'
